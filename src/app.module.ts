5c52e7ce472681c9afc41fcd57b8225caa659566
selestrel07 <selestrel07@gmail.com>
2025-11-25 15:54:13 +0100

    feat: generate track module with nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [UserModule, ArtistModule],
+  imports: [UserModule, ArtistModule, TrackModule],
   controllers: [AppController],
   providers: [AppService],
 })
