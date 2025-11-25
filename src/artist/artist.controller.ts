7a2263bd36965526c519662a6790f5f713d205bd
selestrel07 <selestrel07@gmail.com>
2025-11-25 15:35:01 +0100

    feat: implement artist create functionality

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { Artist } from './dto/artist.dto';
 import { PathParameters } from 'src/interfaces/path-params';
 import { validateId } from 'src/utils/validateId';
+import { CreateArtistDto } from './dto/create-artist.dto';
 
 @Controller('artist')
 export class ArtistController {
@@ -17,5 +18,13 @@ export class ArtistController {
   findById(@Param() params: PathParameters) {
     validateId(params.id);
     return this.artistService.findById(params.id);
+  }
+
+  @Post()
+  create(@Body() body: CreateArtistDto) {
+    if (!('name' in body && 'grammy' in body) || !(typeof body.name === 'string' && typeof body.grammy === 'boolean')) {
+      throw new HttpException('Request body does not contain all required fields (name, grammy) or some field is empty or has wrong data type', HttpStatus.BAD_REQUEST);
+    }
+    return this.artistService.create(body);
   }
 }
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,5 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Artist } from './dto/artist.dto';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class ArtistService {
@@ -14,6 +16,20 @@ export class ArtistService {
     if (!artist) {
       throw new HttpException(`Artist with id '${id}' was not found`, HttpStatus.NOT_FOUND);
     }
+    return artist;
+  }
+
+  create(createArtistDto: CreateArtistDto): Artist {
+    let id = '';
+    while (id.length === 0 || this.artists.find((a) => a.id === id) !== undefined) {
+      id = randomUUID();
+    }
+    const artist = new Artist({
+      id,
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    });
+    this.artists.push(artist);
     return artist;
   }
 }
