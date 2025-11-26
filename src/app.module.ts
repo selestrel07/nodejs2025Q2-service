bf397e12c9084191691ae65d150bb03d5d3bf9a0
selestrel07 <selestrel07@gmail.com>
2025-11-26 13:46:33 +0100

    feat: generate favs resource with nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [UserModule, ArtistModule, TrackModule, AlbumModule],
+  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/favs/favs.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { FavsService } from './favs.service';
+
+@Controller('favs')
+export class FavsController {
+  constructor(private readonly favsService: FavsService) {}
+}
